11f7dbd8c9cd3fdd877ec6ba1e8f727be913381e
Floresa Zogaj <floresaa.zogaj@gmail.com>
2024-08-01 07:36:08 +0200

    Placed button in bottom position

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({ version, date, label, labelColor, author, authorImage, description }) => {
   return (
-    <div className="bg-white shadow-lg rounded-lg p-6 ml-z">
+    <div className="bg-white shadow-lg rounded-lg p-6 flex flex-col h-full">
       <div className="flex items-center justify-between mb-4">
         <span className="text-gray-500">{version} ({date})</span>
       </div>
@@ -19,10 +19,12 @@ const Card = ({ version, date, label, labelColor, author, authorImage, descripti
           </div>
         </div>
       </div>
-      <p className="text-gray-700 text-base mb-4 whitespace-pre-line">{description}</p>
-      <button className="bg-white text-indigo-500 border border-indigo-500 px-4 py-2 rounded-lg">
-        Download
-      </button>
+      <p className="text-gray-700 text-base mb-4 whitespace-pre-line flex-grow">{description}</p>
+      <div className="mt-auto">
+        <button className="bg-white text-indigo-500 border border-indigo-500 px-4 py-2 rounded-full w-32" style={{ borderRadius: '100vmax' }}>
+          Download
+        </button>
+      </div>
     </div>
   );
 };
